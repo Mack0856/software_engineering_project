63fa825bd7df254f4531c18406aee30c56a7a8c6
Mack0856 <mkk0856@gmail.com>
2014-10-13 23:04:44 +0200

    Added Api Controllers

---
+++ app/controllers/api/invoices_controller.rb
@@ -0,0 +1,71 @@
+class Api::InvoicesController < ApplicationController
+  before_action :set_invoice, only: [:show, :edit, :update, :destroy]
+
+  # GET /invoices
+  # GET /invoices.json
+  def index
+    @invoices = Invoice.all
+    render json: @invoices
+  end
+
+  # GET /invoices/1
+  # GET /invoices/1.json
+  def show
+  end
+
+  # GET /invoices/new
+  def new
+    @invoice = Invoice.new
+    render json: @invoice
+  end
+
+  # GET /invoices/1/edit
+  def edit
+  end
+
+  # POST /invoices
+  # POST /invoices.json
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    respond_to do |format|
+      if @invoice.save
+        format.json { render :show, status: :created, location: @invoice }
+      else
+        format.json { render json: @invoice.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /invoices/1
+  # PATCH/PUT /invoices/1.json
+  def update
+    respond_to do |format|
+      if @invoice.update(invoice_params)
+        format.json { render :show, status: :ok, location: @invoice }
+      else
+        format.json { render json: @invoice.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /invoices/1
+  # DELETE /invoices/1.json
+  def destroy
+    @invoice.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_invoice
+      @invoice = Invoice.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def invoice_params
+      params.require(:invoice).permit(:total_cost, :date_payable, :status)
+    end
+end
---
+++ app/controllers/api/transactions_controller.rb
@@ -0,0 +1,71 @@
+class Api::TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show, :edit, :update, :destroy]
+
+  # GET /transactions
+  # GET /transactions.json
+  def index
+    @transactions = Transaction.all
+    render json: @transactions
+  end
+
+  # GET /transactions/1
+  # GET /transactions/1.json
+  def show
+    render json: @transaction
+  end
+
+  # GET /transactions/new
+  def new
+    @transaction = Transaction.new
+  end
+
+  # GET /transactions/1/edit
+  def edit
+  end
+
+  # POST /transactions
+  # POST /transactions.json
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    respond_to do |format|
+      if @transaction.save
+        format.json { render :show, status: :created, location: @transaction }
+      else
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /transactions/1
+  # PATCH/PUT /transactions/1.json
+  def update
+    respond_to do |format|
+      if @transaction.update(transaction_params)
+        format.json { render :show, status: :ok, location: @transaction }
+      else
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /transactions/1
+  # DELETE /transactions/1.json
+  def destroy
+    @transaction.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def transaction_params
+      params.require(:transaction).permit(:sk_song, :date, :cost)
+    end
+end
