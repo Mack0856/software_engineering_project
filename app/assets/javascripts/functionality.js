68096dfee283ce83764664ca673b3b839796ddc8
Mack0856 <mkk0856@gmail.com>
2014-10-15 09:06:54 +1300

    Login done

--- app/assets/javascripts/functionality.js
+++ app/assets/javascripts/functionality.js
@@ -1,18 +1,26 @@
-
+$(function() {
   // $('.dropdown-toggle').dropdown();
 
   $('.dropdown input, .dropdown label').click(function(e) {
     e.stopPropagation();
   });
 
-  $('.bnt.btn-default.sign-in').click(function(e) {
-    alert("HOSH!!!");
+  $('.sign-in').click(function(e) {
     $.ajax({
       type: "POST",
       url: "/login",
       data: {
         email: $('#user').val(),
         password: $('#password').val()
+      },
+      statusCode: {
+        200: function(status){
+          window.location = '/home'
+        },          
+        401: function(){
+
+        }
       }
     })
   })      
+});
