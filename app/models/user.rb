8fa5e83e17de519096bf6ca2694a3aa37b87757d
vagrant <vagrant@precise64.(none)>
2014-10-25 11:58:22 +0000

    Moved singup into landing index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+	validates :password, presence: true
+	validates :address_line_1, presence: true
+	validates :postcode, presence: true
+	validates :gender, presence: true
+	validates :date_of_birth, presence: true
+
 end
