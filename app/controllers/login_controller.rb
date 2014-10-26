a407b504fbfe23790d4d04eb28e4f312aa007676
Mack0856 <mkk0856@gmail.com>
2014-10-26 20:16:52 +0000

    Fix derp

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,7 @@
 class LoginController < ApplicationController
   def login
     user = User.find_by_email(params[:email])
-    if user && user.password == params[:password] && user.suspended == false
+    if user && user.password == params[:password]
     	session[:user_id] = user.id
       render json: { id: user.id }, status: :ok
     else
