ccbee43edb89ecde1d9ef46a5e663b6b61436b94
Mack0856 <mkk0856@gmail.com>
2014-10-26 11:39:47 +0000

    Implemented sessions, json of album art, subscriptions

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,6 +2,7 @@ class LoginController < ApplicationController
   def login
     user = User.find_by_email(params[:email])
     if user && user.password == params[:password]
+    	session[:user_id] = user.id
       render json: { id: user.id }, status: :ok
     else
       render json: "", status: :unauthorized
